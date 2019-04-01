43b5eda7df49900925014d1e91547090913df759
André Giampaolo <andregiampaolo@gmail.com>
2019-04-01 20:42:36 -0300

    Create a field to make a tweet

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
 export default class Timeline extends Component {
+    state = {
+        newTweet: ''
+    };
+
+    handleInputChange = (e) => {
+        this.setState({newTweet: e.target.value});
+    }
+
+    handleKeyDown = (e) => {
+        if(e.keyCode !== 13) return;
+        const content = this.state.newTweet;
+        const author = localStorage.getItem('@Twitter:username');
+        console.log(content, author);
+    };
+
     render(){
         return(
-            <h1>Linha do tempo</h1>
+            <div className="timeline-wrapper">
+                <h1>Timeline</h1>
+                <form>
+                    <textarea
+                        value={this.state.newTweet}
+                        onChange={this.handleInputChange}
+                        onKeyDown={this.handleKeyDown}
+                        placeholder="O que você está pensando?"
+                        >
+
+                    </textarea>
+                </form>
+            </div>
         )
     }
 }
