eeb12cb789655f8b68299d938687aa96d3301bf2
André Giampaolo <andregiampaolo@gmail.com>
2019-04-01 20:23:38 -0300

    Treating username on state variable

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+
+    state = {
+        username: '',
+    };
+
+    handleInputChange = (e) => {
+        this.setState({username: e.target.value});
+    }
+
     render(){
         return(
             <div className="login-wrapper">
                 <h1>Login</h1>
                 <form>
-                    <input type="text" placeholder="Nome do usuário" />
+                    <input 
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        type="text" 
+                        placeholder="Nome do usuário" 
+                        />
                     <button type="submit">Entrar</button>
                 </form>
             </div>
